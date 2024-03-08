ad4ed429af38ba54a0619e486f62a7c5642b973a
Alvmoc <amcfuturex@gmail.com>
2024-03-08 16:32:37 +0000

    Maquetación menú principal, añadir contraseña (apps and tags).

--- AKMAndroidClient/app/build.gradle.kts
+++ AKMAndroidClient/app/build.gradle.kts
@@ -54,7 +54,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
