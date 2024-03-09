39ea46a99e22088e2fa8be2677ab8b523d003456
Manuel Prado <manguel.corocotta@gmail.com>
2024-03-09 10:56:34 +0100

    validaciones para PageSignIn y PageSignUp

--- AKMAndroidClient/app/src/main/java/com/linkiaM13G3/akmAndroidClient/Pages/PageSignIn.kt
+++ AKMAndroidClient/app/src/main/java/com/linkiaM13G3/akmAndroidClient/Pages/PageSignIn.kt
@@ -1,12 +1,88 @@
 package com.linkiaM13G3.akmAndroidClient.Pages
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import com.linkiaM13G3.akmAndroidClient.R
 
 class PageSignIn : AppCompatActivity() {
+
+    private lateinit var usernameOrEmailInputLayout: TextInputLayout
+    private lateinit var passwordInputLayout: TextInputLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.page_sign_in);
+        setContentView(R.layout.page_sign_in)
+
+        // Inicializa las vistas
+        usernameOrEmailInputLayout = findViewById(R.id.editText)
+        passwordInputLayout = findViewById(R.id.textInputLayout)
+
+        val signInButton: Button = findViewById(R.id.buttonSignIn)
+        val signUpButton: Button = findViewById(R.id.buttonSignUp)
+
+        // Configura el listener para el botón de inicio de sesión
+        signInButton.setOnClickListener {
+            performSignIn()
+        }
+
+        // Configura el listener para el botón de registro
+        signUpButton.setOnClickListener {
+            navigateToSignUp()
+        }
+    }
+
+    private fun performSignIn() {
+        val usernameOrEmail = usernameOrEmailInputLayout.editText?.text.toString().trim()
+        val password = passwordInputLayout.editText?.text.toString().trim()
+
+        // Valida el nombre de usuario o correo y la contraseña
+        val isUsernameOrEmailValid = validateUsernameOrEmail(usernameOrEmail, usernameOrEmailInputLayout)
+        val isPasswordValid = validatePassword(password, passwordInputLayout)
+
+        // Si ambos son válidos, procede con la lógica de inicio de sesión
+        if (isUsernameOrEmailValid && isPasswordValid) {
+            // Lógica de inicio de sesión
+        }
+    }
+
+    private fun navigateToSignUp() {
+        // Navegar a la actividad de registro
+        val signUpIntent = Intent(this, PageSignUp::class.java)
+        startActivity(signUpIntent)
+    }
+
+    private fun validateUsernameOrEmail(input: String, textInputLayout: TextInputLayout): Boolean {
+        val isValidEmail = input.contains("@") && Patterns.EMAIL_ADDRESS.matcher(input).matches()
+        val isValidUsername = input.matches(Regex("^[a-zA-Z0-9]{2,10}$"))
+
+        if (!isValidEmail && !isValidUsername) {
+            textInputLayout.error = "Enter a valid username or email address"
+            return false
+        } else {
+            textInputLayout.error = null
+            return true
+        }
+    }
+
+    private fun validatePassword(password: String, textInputLayout: TextInputLayout): Boolean {
+        val hasUppercase = password.any { it.isUpperCase() }
+        val hasNumber = password.any { it.isDigit() }
+        if (password.length < 8) {
+            textInputLayout.error = "Password must be at least 8 characters"
+            return false
+        } else if (!hasUppercase) {
+            textInputLayout.error = "Password must have at least one uppercase letter"
+            return false
+        } else if (!hasNumber) {
+            textInputLayout.error = "Password must have at least one number"
+            return false
+        } else {
+            textInputLayout.error = null
+            return true
+        }
     }
 }
--- AKMAndroidClient/app/src/main/java/com/linkiaM13G3/akmAndroidClient/Pages/PageSignUp.kt
+++ AKMAndroidClient/app/src/main/java/com/linkiaM13G3/akmAndroidClient/Pages/PageSignUp.kt
@@ -1,25 +1,126 @@
 package com.linkiaM13G3.akmAndroidClient.Pages
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.util.Patterns
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputLayout
 import com.linkiaM13G3.akmAndroidClient.R
 
 class PageSignUp : AppCompatActivity() {
-    private lateinit var spinnerCountryCode : Spinner
-
-    private fun initializeComponents() {
-        spinnerCountryCode = findViewById(R.id.spinnerCountryCode)
-        spinnerCountryCode.adapter = ArrayAdapter(
-            this,
-            android.R.layout.simple_spinner_dropdown_item,
-            resources.getStringArray(R.array.country_codes)
-        )
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.page_sign_up)
-        initializeComponents()
+
+        val buttonSignUp: Button = findViewById(R.id.buttonSignUp)
+        val textInputFName: TextInputLayout = findViewById(R.id.textInputFName)
+        val textInputLName: TextInputLayout = findViewById(R.id.textInputLName)
+        val emailTextInputLayout: TextInputLayout = findViewById(R.id.textInputMail)
+        val telephoneTextInputLayout: TextInputLayout = findViewById(R.id.textInputTelephone)
+        val usernameTextInputLayout: TextInputLayout = findViewById(R.id.textInputUsername)
+        val passwordTextInputLayout: TextInputLayout = findViewById(R.id.textInputPassword)
+        val rePasswordTextInputLayout: TextInputLayout = findViewById(R.id.textInputREPassword)
+
+        buttonSignUp.setOnClickListener {
+            val fName = textInputFName.editText?.text.toString().trim()
+            val lName = textInputLName.editText?.text.toString().trim()
+            val email = emailTextInputLayout.editText?.text.toString().trim()
+            val telephone = telephoneTextInputLayout.editText?.text.toString().trim()
+            val usernameOrEmail = usernameTextInputLayout.editText?.text.toString().trim()
+            val password = passwordTextInputLayout.editText?.text.toString().trim()
+            val rePassword = rePasswordTextInputLayout.editText?.text.toString().trim()
+
+            val isValidFName = validateName(fName, textInputFName)
+            val isValidLName = validateName(lName, textInputLName)
+            val isValidEmail = validateEmail(email, emailTextInputLayout)
+            val isValidTelephone = validateTelephone(telephone, telephoneTextInputLayout)
+            val isValidUsernameOrEmail = validateUsernameOrEmail(usernameOrEmail, usernameTextInputLayout)
+            val isValidPassword = validatePassword(password, passwordTextInputLayout)
+            val isPasswordMatch = validateRePassword(password, rePassword, rePasswordTextInputLayout)
+
+            if (isValidFName && isValidLName && isValidEmail && isValidTelephone && isValidUsernameOrEmail && isValidPassword && isPasswordMatch) {
+                // Todo está bien, proceder con el registro
+            }
+        }
+    }
+
+    private fun validateName(name: String, textInputLayout: TextInputLayout): Boolean {
+        textInputLayout.error = null // Limpia errores previos
+
+        when {
+            name.isEmpty() -> {
+                textInputLayout.error = "Field cannot be empty"
+                return false
+            }
+            name.length < 2 || name.length > 50 -> {
+                textInputLayout.error = "Name must be between 2 and 50 characters"
+                return false
+            }
+            !name.all { it.isLetter() || it == '-' || it == '\'' } -> {
+                textInputLayout.error = "Name contains invalid characters"
+                return false
+            }
+
+        }
+
+        return true
     }
 }
+    private fun validateEmail(email: String, textInputLayout: TextInputLayout): Boolean {
+        if (email.isEmpty()) {
+            textInputLayout.error = "Email is required"
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            textInputLayout.error = "Enter a valid email address"
+            return false
+        } else {
+            textInputLayout.error = null
+            return true
+        }
+    }
+
+    private fun validateTelephone(telephone: String, textInputLayout: TextInputLayout): Boolean {
+        if (telephone.isEmpty()) {
+            textInputLayout.error = "Telephone number is required"
+            return false
+        } else if (!telephone.matches(Regex("^[+]?\\d{10,15}$"))) {
+            textInputLayout.error = "Enter a valid telephone number"
+            return false
+        } else {
+            textInputLayout.error = null
+            return true
+        }
+    }
+
+    private fun validateUsernameOrEmail(input: String, textInputLayout: TextInputLayout): Boolean {
+        val isValidEmail = input.contains("@") && Patterns.EMAIL_ADDRESS.matcher(input).matches()
+        val isValidUsername = input.matches(Regex("^[a-zA-Z]{2,10}$"))
+
+        if (!isValidEmail && !isValidUsername) {
+            textInputLayout.error = "Enter a valid username or email address"
+            return false
+        } else {
+            textInputLayout.error = null
+            return true
+        }
+    }
+
+    private fun validatePassword(password: String, textInputLayout: TextInputLayout): Boolean {
+        if (password.length < 8) {
+            textInputLayout.error = "Password must be at least 8 characters"
+            return false
+        } else {
+            textInputLayout.error = null
+            return true
+        }
+    }
+
+    private fun validateRePassword(password: String, rePassword: String, textInputLayout: TextInputLayout): Boolean {
+        if (password != rePassword) {
+            textInputLayout.error = "Passwords do not match"
+            return false
+        } else {
+            textInputLayout.error = null
+            return true
+        }
+    }
